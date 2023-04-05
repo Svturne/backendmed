25a4e3e5754bf376ff85c942676b7baba0193fae
Svturne <mehdi.bad@outook.com>
2023-04-05 11:25:18 +0200

    finished maladie end point

--- controller/maladie.js
+++ controller/maladie.js
@@ -1,5 +1,6 @@
 const Maladie = require("../model/Maladie");
 const client = require("../bd/connect");
+const { ObjectId } = require("mongodb");
 
 const addMaladie = async (req, res) => {
   try {
@@ -31,4 +32,20 @@ const getAllMaladie = async (req, res) => {
   }
 };
 
-module.exports = { addMaladie, getAllMaladie };
+const updateMaladie = async (req, res) => {
+  try {
+    const id = new ObjectId(req.params.id);
+    const newMaladie = req.body.maladie;
+    let result = await client
+      .bd()
+      .collection("maladies")
+      .updateOne({ _id: id }, { $set: { maladie: newMaladie } });
+    res.status(200).json(result);
+  } catch (error) {
+    console.log("erreur in update maladie");
+    console.log(error);
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { addMaladie, getAllMaladie, updateMaladie };
--- route/maladie.js
+++ route/maladie.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const { addMaladie, getAllMaladie } = require("../controller/maladie");
+const {
+  addMaladie,
+  getAllMaladie,
+  updateMaladie,
+} = require("../controller/maladie");
 const router = express.Router();
 
 router.route("/maladie").post(addMaladie);
 router.route("/maladie").get(getAllMaladie);
+router.route("/maladie/:id").put(updateMaladie);
 
 module.exports = router;
