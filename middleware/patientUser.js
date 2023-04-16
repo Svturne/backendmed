78623a23a2a50dc071d545dd7b0b9a03087214e2
Svturne <mehdi.bad@outook.com>
2023-04-16 09:58:00 +0100

    fix token verif

--- middleware/patientUser.js
+++ middleware/patientUser.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const client = require("../bd/connect");
 
 const patientFirstAuth = async (req, res, next) => {
   let token = req.headers["authorization"];
@@ -6,6 +7,16 @@ const patientFirstAuth = async (req, res, next) => {
     return res.status(401).json({ message: "not authorized" });
   }
   token = token.split(" ")[1];
+
+  const result = await client
+    .bd()
+    .collection("tokensPatient")
+    .findOne({ token });
+  console.log(result);
+
+  if (!result) {
+    return res.status(401).json({ message: "token invalide" });
+  }
 
   jwt.verify(token, process.env.REFRESHTOKENPATIENT, (err, user) => {
     if (!err) {
